8b2fc54edfaf7dda5244c5df717bd48745af5deb
MannyPV <mpvalecillos@knights.ucf.edu>
2022-10-15 17:37:31 -0400

    Laps and prefabs

---
+++ Assets/Script/PlayerController/LapScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LapScript : MonoBehaviour
+{   
+    public int TotalLaps= 3;
+    public int CurrentLaps=1;
+
+    bool CanDoLaps;
+    float timeBetweenLaps;
+    public float minTimeBetweenLaps = 30f;
+
+    public Text LapDisplayText;
+
+    public bool isPlayer;
+    void Update()
+    {
+        if(timeBetweenLaps <= 0)
+        {
+            CanDoLaps = true;
+            timeBetweenLaps= minTimeBetweenLaps;
+        }
+        else
+        {
+            timeBetweenLaps-= Time.deltaTime;
+        }
+
+        if(isPlayer)
+        {
+            LapDisplayText.text = "Laps: " + CurrentLaps+ "/" + TotalLaps;
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Lap" && CanDoLaps)
+        {
+            CurrentLaps +=1;
+            CanDoLaps = false;
+        }
+    }
+}
