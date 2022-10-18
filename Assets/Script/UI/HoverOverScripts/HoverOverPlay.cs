c0d3f2bc9f7a7e5ce52803b41387474b2680ea13
Ben Rochberg <benrochberg@gmail.com>
2022-10-18 14:59:19 -0400

    Main Menu Added Button Texts
    -Hovering over the Play and Records buttons in the Main Menu will reveal a couple small text objects.
    -Moved all HoverOver Button scripts to a new folder.

---
+++ Assets/Script/UI/HoverOverScripts/HoverOverPlay.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverOverPlay : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject _playBtn, _playBackground, _playAddedTxtOne, _playAddedTxtTwo;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        LeanTween.scale(_playBtn, new Vector3(1.1f, 1.1f, 1.1f), 0.01f).setEase(LeanTweenType.easeOutExpo);
+        LeanTween.scale(_playAddedTxtOne, new Vector3(1f, 1f, 1f), 0.15f).setEase(LeanTweenType.easeOutExpo);
+        LeanTween.scale(_playAddedTxtTwo, new Vector3(1f, 1f, 1f), 0.15f).setEase(LeanTweenType.easeOutExpo);
+        LeanTween.alpha(_playBackground.GetComponent<RectTransform>(), 0.9f, 0.1f);  
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        LeanTween.scale(_playBtn, new Vector3(1f, 1f, 1f), 0.01f).setEase(LeanTweenType.easeInExpo);
+        LeanTween.scale(_playAddedTxtOne, new Vector3(1f, 0f, 1f), 0.08f).setEase(LeanTweenType.easeInExpo);
+        LeanTween.scale(_playAddedTxtTwo, new Vector3(1f, 0f, 1f), 0.08f).setEase(LeanTweenType.easeInExpo);
+        LeanTween.alpha(_playBackground.GetComponent<RectTransform>(), 0f, 0.1f);
+    }
+}
