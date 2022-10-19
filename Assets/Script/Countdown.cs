2a5727074bef5af66311588b4b61b97473407dff
rivaUCF <richard_vanderhoof@knights.ucf.edu>
2022-10-18 21:29:47 -0400

    321GO

---
+++ Assets/Script/Countdown.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    float timer;
+    public Text countdown;
+    public Image countBack;
+
+    public GameObject player;
+    CarDrive driveScript;
+    CarTurn turnScript;
+
+    bool clearCounter;
+    float clearTimer;
+
+    void Start()
+    {
+        timer = 8;
+
+        countdown.text = "...";
+        countdown.color = Color.white;
+
+        driveScript = player.GetComponent<CarDrive>();
+        turnScript = player.GetComponent<CarTurn>();
+
+        clearCounter = false;
+        clearTimer = 0;
+    }
+
+    private void Update()
+    {
+        if((int)timer >= 4)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if ((int)timer > 0 && (int)timer < 4)
+        {
+            timer -= Time.deltaTime;
+
+            if ((int)timer != 0)
+            {
+                countdown.color = Color.red;
+                countdown.text = "" + ((int)timer);
+            }
+        }
+        else
+        {
+            countdown.text = "GO!";
+            countdown.color = Color.green;
+            GivePlayerControl();
+            timer = 0;
+
+            clearCounter = true;
+        }
+
+        if(clearCounter == true)
+        {
+            ClearCounterUI();
+        }
+    }
+
+    void GivePlayerControl()
+    {
+        driveScript.enabled = true;
+        turnScript.enabled = true;
+    }
+
+    void ClearCounterUI()
+    {
+        clearTimer += Time.deltaTime;
+        if(clearTimer > 2)
+        {
+            countdown.enabled = false;
+            countBack.enabled = false;
+            enabled = false;
+        }
+    }
+}
