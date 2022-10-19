65b0290b0431f10d48f865d3c536ac693778c7d1
rivaUCF <richard_vanderhoof@knights.ucf.edu>
2022-10-18 20:22:36 -0400

    Speedometer

--- Assets/Script/PlayerController/CarDrive.cs
+++ Assets/Script/PlayerController/CarDrive.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarDrive : MonoBehaviour
 {
@@ -8,12 +9,14 @@ public class CarDrive : MonoBehaviour
 
     public float Speed = 8f;
     public int multiplier = 10;
-     public float gravityMultiplier = 5f;
+    public float gravityMultiplier = 5f;
+
+    public Text speedometer; // The speed text on the UI
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); 
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -41,10 +44,18 @@ public class CarDrive : MonoBehaviour
         Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
         localVelocity = new Vector3(0,localVelocity.y,localVelocity.z);
         rb.velocity = new Vector3(transform.TransformDirection(localVelocity).x,rb.velocity.y , transform.TransformDirection(localVelocity).z);
+
+        RecordSpeed(); // New function below to update ui text
     }
 
     void Fall()
     {
         rb.AddForce(Vector3.down * gravityMultiplier * multiplier);
+    }
+
+    void RecordSpeed()
+    {
+        speedometer.text = "Speed: " + (int)rb.velocity.magnitude*3;
+        // Changes the text on the UI to whatever the speed is as an int. *3 because the numbers make more sense that way.
     }
 }
